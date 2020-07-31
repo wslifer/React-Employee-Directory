00429b56c8f3e79d5c8663b84860e1970cfa6b29
Wesley Slifer <wesleyslifer@ymail.com>
2020-07-31 12:08:16 -0500

    adding table component

---
+++ src/components/Table/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Search from "../Search/index";
+
+function Table({
+  toDisplay,
+  handleFormSubmit,
+  handleInputChange,
+  handleButtonClick,
+}) {
+  return (
+    <main className="wrapper">
+      <Search
+        handleFormSubmit={handleFormSubmit}
+        handleInputChange={handleInputChange}
+      />
+      <div className="table"></div>
+    </main>
+  );
+}
