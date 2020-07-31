0eebedd0cc31244c27d6f54c1f63deadec18fda3
Wesley Slifer <wesleyslifer@ymail.com>
2020-07-31 12:23:45 -0500

    updating homepage page

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -8,6 +8,56 @@ class Homepage extends Component {
     toDisplay: [],
     sort: "none",
   };
+  handleInputChange = (e) => {
+    this.setState({ search: e.target.value });
+  };
+  componentDidMount() {
+    API.getUsers().then(({ data }) => {
+      this.setState({
+        users: data.results,
+        toDisplay: data.results,
+      });
+    });
+  }
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      toDisplay: this.state.users.filter(
+        (user) =>
+          user.name.first
+            .toLowerCase()
+            .includes(this.state.search.toLowerCase()) ||
+          user.name.last.toLowerCase().includes(this.state.search.toLowerCase())
+      ),
+    });
+  };
+  handleButtonClick = () => {
+    if (this.state.sort === "none" || this.state.sort === "down") {
+      this.setState({
+        sort: "up",
+        toDisplay: this.state.toDisplay.sort((a, b) => a.dob.age - b.dob.age),
+      });
+    } else {
+      this.setState({
+        sort: "down",
+        toDisplay: this.state.toDisplay.sort((a, b) => b.dob.age - a.dob.age),
+      });
+    }
+  };
+  render() {
+    return (
+      <div>
+        <Header />
+        <Table
+          toDisplay={this.state.toDisplay}
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange}
+          handleButtonClick={this.handleButtonClick}
+        />
+      </div>
+    );
+  }
 }
 
 export default Homepage;
