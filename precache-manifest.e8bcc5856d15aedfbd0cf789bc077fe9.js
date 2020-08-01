c4209559acaddbb8a3ea05fcd80c4be2b16bc5ca
Wesley Slifer <wesleyslifer@ymail.com>
2020-08-01 10:57:28 -0500

    Updates

---
+++ precache-manifest.e8bcc5856d15aedfbd0cf789bc077fe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853b3b4a00675ded4a936e7711d72b74",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "c1184b2ed7d73a0f9179",
+    "url": "/React-Employee-Directory/static/css/main.c0c96fc9.chunk.css"
+  },
+  {
+    "revision": "d79c10c9cc1453e1fee8",
+    "url": "/React-Employee-Directory/static/js/2.0c9e4a3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Employee-Directory/static/js/2.0c9e4a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1184b2ed7d73a0f9179",
+    "url": "/React-Employee-Directory/static/js/main.0a98bd73.chunk.js"
+  },
+  {
+    "revision": "7a3b04eb39eeeb27bc59",
+    "url": "/React-Employee-Directory/static/js/runtime-main.a0389cb5.js"
+  }
+]);
