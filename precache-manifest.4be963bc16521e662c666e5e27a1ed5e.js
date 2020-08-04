88d08dbf1f0c2c72dfbdeaeab118393aa22fa242
wslifer <wesleyslifer@ymail.com>
2020-08-04 17:57:58 -0500

    Updates

--- precache-manifest.4be963bc16521e662c666e5e27a1ed5e.js
+++ precache-manifest.4be963bc16521e662c666e5e27a1ed5e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "853b3b4a00675ded4a936e7711d72b74",
+    "revision": "8ef1235573254f3eb11a507a3e078ca6",
     "url": "/React-Employee-Directory/index.html"
   },
   {
-    "revision": "c1184b2ed7d73a0f9179",
-    "url": "/React-Employee-Directory/static/css/main.c0c96fc9.chunk.css"
+    "revision": "e37831890556f8ff7039",
+    "url": "/React-Employee-Directory/static/css/main.5e90f64c.chunk.css"
   },
   {
     "revision": "d79c10c9cc1453e1fee8",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React-Employee-Directory/static/js/2.0c9e4a3a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c1184b2ed7d73a0f9179",
-    "url": "/React-Employee-Directory/static/js/main.0a98bd73.chunk.js"
+    "revision": "e37831890556f8ff7039",
+    "url": "/React-Employee-Directory/static/js/main.c1035669.chunk.js"
   },
   {
     "revision": "7a3b04eb39eeeb27bc59",
